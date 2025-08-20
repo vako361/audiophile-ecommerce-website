4c49fc2329745ed8253764d54d6e69799ea77c0f
vako <vakokoblianidze12334@gmail.com>
2025-08-21 01:43:44 +0400

    commit

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -2,10 +2,11 @@ import { useParams } from "react-router-dom"
 import data from "../data/data.json"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { useState } from "react"
 
 export default function Product() {
-  const { productId} = useParams()
-
+  const { productId, category } = useParams()
+  const [quantity, setQuantity] = useState<number>(0)
   const product = data.find(
     (item) => item.id === Number(productId) 
   )
@@ -20,17 +21,36 @@ export default function Product() {
   }
 
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col ">
       <Header />
-   <div className="flex flex-col pl-4 pr-4 gap-8 justify-center">
+   <div className="flex flex-col pl-4 pr-4 gap-8 justify-center mt-16">
     <img src={product.image.mobile} alt="" className="rounded-[8px]" />
     <p className="text-[#D87D4A] text-[14px] tracking-[14px]">NEW PRODUCT</p>
     <h2 className="text-[28px] font-[700] text-black ">{product.name}</h2>
     <p className="opacity-50 leading-6">{product.description}</p>
     <span className="text-[18px] font-[700]">{`$ ${product.price}`}</span>
-    <div className="flex ">
-      <button className="text-white bg-[#D87D4A] w-[160px] h-[48px]">ADD TO CART</button>
+    <div className="flex gap-4">
+      <div className="flex items-center justify-around w-[120px] h-[48px] bg-[#F1F1F1]">
+        <span className="cursor-pointer" onClick={() => quantity !== 0 && setQuantity(quantity - 1)} >-</span>
+        {quantity}
+        <span className="cursor-pointer" onClick={() => setQuantity(quantity + 1)}>+</span>
       </div>
+      <button className="text-white bg-[#D87D4A] w-[160px] h-[48px]">ADD TO CART</button>
+    </div>
+
+    
+    <section className="flex flex-col gap-6"> 
+      <h2 className="text-black text-[24px] font-[700]">FEATURES</h2>
+      <p  className="opacity-50 tracking-[0.87px]">{product.features}</p>
+
+    </section>
+
+    <div className="flex flex-col">
+      <h2 className="font-[700] text-[24px] uppercase">in the  box</h2>
+      <p><span></span></p>
+      <p><span></span></p>
+    </div>
+
    </div> 
  <Footer />
     </section>
